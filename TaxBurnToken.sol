2c5911ba927ad5a23856a1a89f2bbb8a927999b1
tangojo <31540856+tangojo@users.noreply.github.com>
2021-03-27 08:06:27 +0100

    Update and rename TokenTest3.sol to TaxBurnToken.sol
    2

--- TaxBurnToken.sol
+++ TaxBurnToken.sol
@@ -1,20 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.2;
 
-contract Token {
+contract TaxBurnToken {
     mapping(address => uint) public balances;
     mapping(address => mapping(address => uint)) public allowance;
     uint public totalSupply = 1000000 * 10 ** 18;
-    string public name = "UpOnlyToken";
-    string public symbol = "UOT";
+    string public name = "TaxBurnToken";
+    string public symbol = "TBT";
     uint public decimals = 18;
     uint public transactionTax = 5;
+    address public burnaddress = 0x0000000000000000000000000000000000000001;
+    
     
     event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
     
     constructor() {
         balances[msg.sender] = totalSupply;
+    }
+    
+    function calculateTaxFee(uint amount) private view returns (uint) {
+        return (amount*(100-transactionTax))/100;
     }
     
     function balanceOf(address owner) public view returns (uint) {
@@ -24,14 +30,12 @@ contract Token {
     function transfer(address to, uint value) public returns(bool) {
         require(balanceOf(msg.sender) >= value, 'balance too low');
         // calculate value minus tax
-        uint netValue = (value*(100-transactionTax))/100;
+        uint netValue = calculateTaxFee(value);
         // calculate value to burn
         uint valueToBurn = value - netValue;
         balances[to] = balances[to] + netValue;
         balances[msg.sender] -= value;
         // burn token
-        //totalSupply -= valueToBurn;
-        address burnaddress = 0x0000000000000000000000000000000000000001;
         balances[burnaddress] += valueToBurn;
         emit Transfer(msg.sender, to, netValue);
         emit Transfer(msg.sender, burnaddress, valueToBurn);
