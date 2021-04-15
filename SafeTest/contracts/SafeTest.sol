5bbbb393a368c781ba62d62ca91791cbd8a7c664
Joe <you@example.com>
2021-04-15 11:54:09 +0200

    11

--- SafeTest/contracts/SafeTest.sol
+++ SafeTest/contracts/SafeTest.sol
@@ -8,10 +8,10 @@
    - _liquidityFee = 5; // provides Liquidity to Pancakeswap; original $HATE $LIQ logic
    - _taxFee = 5; // auto distribute to holders; original $RFI logic
    - swapAndLiquifyEnabled = true;
-   - _maxTxAmount = 500 * 10**6 * 10**9; // initially set half a billion
-   - numTokensSellToAddToLiquidity = 500 * 10**6 * 10**9; // initially set half a billion
-‚
-   Changes:
+   - _maxTxAmount = 500 * 10**6 * 10**9; // initially set to half a billion
+   - numTokensSellToAddToLiquidity = 500 * 10**6 * 10**9; // initially set  to half a billion
+
+   Changes made:
    - Pancakeswap Router address to 0xD99D1c33F9fC3444f8101754aBC46c52416550D1 testnet
    
    Notes from dxsale:
